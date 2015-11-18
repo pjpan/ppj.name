b92c9c6b1f9de153ddc067b31bcfdc247850ce12
pjpan <immortalness@gmail.com>
2015-11-19 00:23:39 +0800

    new code

--- ppjwebsite/urls.py
+++ ppjwebsite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from ppjwebsite.views import hello,current_datetime,hours_ahead
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$',hello),
+    url(r'^time/$',current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 ]
